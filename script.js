8411e510fdc868306323876024e93047f61a168c
Danya-qq <qallandbb@gmail.com>
2020-04-16 20:27:27 +0300

    corrected

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", ready);
 const container = document.querySelector('.container');
-console.log(container);
 
 function DomElement(selector, height, width, bg, fontSize){
     this.selector = selector;
@@ -34,8 +32,11 @@ let element1 = new DomElement('.block', 'height: 30px', 'width: 560px', 'backgro
 
 let element2 = new DomElement('.block', 'height: 50px', 'width: 200px', 'background-color: yellow', 'font-size: 40px');
  
+let element3 = new DomElement('.block', 'height: 50px', 'width: 200px', 'background-color: blue', 'font-size: 40px');
+
 element1.addElem('aaaa');
 element2.addElem();
+element3.addElem();
 
 
 
