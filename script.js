d9f3307d4e590d883cc3753a11c617e16bd520e1
Danya-qq <qallandbb@gmail.com>
2020-04-19 21:04:03 +0300

    done

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
-const container = document.querySelector('.container');
+class First {
+    constructor(){}
+    hello(){
+        console.log('Привет! Я имя родителя!');
+        
+    }
+}
 
-function DomElement(selector, height, width, bg, fontSize){
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
+class Second extends First {
+    constructor(){
+        super()
+    }
+    hello(){
+        super.hello()
+        console.log('А я наследуемый метод');   
+    }
+  
 
-};
+}
 
-DomElement.prototype.addElem = function(text = ''){
-    let elem;
-    if (this.selector.startsWith('.')) {
-        elem = document.createElement('div')
-        elem.setAttribute('class', this.selector.slice(1));
-        console.log(elem);
-    } else if (this.selector.startsWith('#')) {
-        elem = document.createElement('p');
-        elem.setAttribute('id', this.selector.slice(1));
-        console.log(elem);
-    } else return;
-   
-    container.append(elem);
+console.dir(Second);
 
-    elem.style.cssText = `${this.height}; ${this.width}; ${this.bg}; ${this.fontSize}`;
-    elem.innerHTML = text; 
-}; 
-
-let element1 = new DomElement('.block', 'height: 30px', 'width: 560px', 'background-color: green', 'font-size: 20px');
-let element2 = new DomElement('#block', 'height: 30px', 'width: 560px', 'background-color: green', 'font-size: 20px');
-
-
-element1.addElem('aaaa');
-element2.addElem('Text')
-
-
+let a = new First;
+a.hello();
+let b = new Second;
+b.hello();
 
 
 
